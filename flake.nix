50f505b6e96bcee0c8dc8f8fb1db420ff99b889a
Harrison Thorne <harrisonthorne@protonmail.com>
2021-11-28 01:02:46 -0600

    flake: Add `overlay` output

--- flake.nix
+++ flake.nix
@@ -10,33 +10,45 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, flake-compat }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    let allSystems =
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        plymouth-theme-musicaloft-rainbow-package =
-          pkgs.stdenv.mkDerivation {
-            pname = "plymouth-theme-musicaloft-rainbow";
-            version = "0.0.1";
+          package =
+            pkgs.stdenv.mkDerivation {
+              pname = "plymouth-theme-musicaloft-rainbow";
+              version = "0.0.1";
 
-            src = ./.;
+              src = ./.;
 
-            buildInputs = [];
+              buildInputs = [ ];
 
-            configurePhase = "mkdir -pv $out/share/plymouth/themes/";
-            dontBuild = true;
-            installPhase = ''
-              ls -l
-              cp -rv musicaloft-rainbow/ $out/share/plymouth/themes/
-              substituteInPlace $out/share/plymouth/themes/musicaloft-rainbow/musicaloft-rainbow.plymouth \
-                --replace "/usr/" "$out/"
-            '';
+              configurePhase = "mkdir -pv $out/share/plymouth/themes/";
+              dontBuild = true;
+              installPhase = ''
+                ls -l
+                cp -rv musicaloft-rainbow/ $out/share/plymouth/themes/
+                substituteInPlace $out/share/plymouth/themes/musicaloft-rainbow/musicaloft-rainbow.plymouth \
+                  --replace "/usr/" "$out/"
+              '';
+            };
+        in
+        {
+          packages.plymouth-theme-musicaloft-rainbow = package;
+          defaultPackage = package;
+          overlay = final: prev: {
+            plymouth-theme-musicaloft-rainbow = package;
           };
-      in
-      {
-        packages.plymouth-theme-musicaloft-rainbow =
-          plymouth-theme-musicaloft-rainbow-package;
-        defaultPackage = plymouth-theme-musicaloft-rainbow-package;
-      }
-    );
+        }
+      );
+    in
+    {
+      packages = allSystems.packages;
+      defaultPackage = allSystems.defaultPackage;
+      overlay = final: prev: {
+        plymouth-theme-musicaloft-rainbow =
+          allSystems.packages.${final.system}.plymouth-theme-musicaloft-rainbow;
+      };
+    };
 }
